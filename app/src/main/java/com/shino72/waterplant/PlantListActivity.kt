2ae2f525790c7f38b99ed7ba6d7b240f905b5335
신명철 <a36243845@gmail.com>
2023-06-17 14:54:52 +0900

    mainlayout 구성

---
+++ app/src/main/java/com/shino72/waterplant/PlantListActivity.kt
@@ -0,0 +1,11 @@
+package com.shino72.waterplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlantListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_list)
+    }
+}
