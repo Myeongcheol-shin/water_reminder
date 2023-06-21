6d97f8e0e4765d2546b10b9dcc30eae4627f21cb
신명철 <a36243845@gmail.com>
2023-06-21 23:15:13 +0900

    설정 + 물주기 및 이름 변경 완성

--- app/src/main/java/com/shino72/waterplant/main/MainActivity.kt
+++ app/src/main/java/com/shino72/waterplant/main/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.shino72.waterplant.main
 
+import android.app.ActionBar.LayoutParams
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.Observer
 import com.shino72.waterplant.R
 import com.shino72.waterplant.databinding.ActivityMainBinding
@@ -37,10 +44,14 @@ class MainActivity : AppCompatActivity() {
             .create()
 
         initSlider()
+        initWaterDialog()
+        initNameDialog()
 
         // binding
         binding.apply {
             settingBtn.setOnClickListener {
+                settingContentView.findViewById<EditText>(R.id.now_et).setText(binding.nowTv.text)
+                settingContentView.findViewById<EditText>(R.id.goal_et).setText(binding.goalTv.text)
                 settingSlideUpPopup.show()
             }
         }
@@ -55,6 +66,7 @@ class MainActivity : AppCompatActivity() {
             })
             name.observe(this@MainActivity, Observer {
                 if(it != null) binding.plantNameTv.text = it.toString()
+                else binding.plantNameTv.text = "이름을 지어주세요"
             })
             wave.observe(this@MainActivity, Observer {
                 binding.waveView.setProgress(it)
@@ -62,6 +74,69 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun initNameDialog()
+    {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_edit_name, null)
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        val alertDialog = builder.create()
+        binding.editNameBtn.setOnClickListener {
+            showEditNameDialog(dialogView, alertDialog)
+        }
+
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        alertDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alertDialog.setContentView(R.layout.dialog_give_water)
+    }
+    private fun showEditNameDialog(view : View, dialog: AlertDialog)
+    {
+        dialog.show()
+        view.findViewById<AppCompatButton>(R.id.cancel_btn).setOnClickListener {
+            dialog.dismiss()
+        }
+        view.findViewById<AppCompatButton>(R.id.apply_btn).setOnClickListener {
+            val prefs = MyApplication.pref
+            val nameValue = view.findViewById<EditText>(R.id.name_et).text.toString()
+            if(nameValue != null){
+                prefs.setName(nameValue)
+                viewModel.valueChange()
+            }
+            dialog.dismiss()
+        }
+    }
+
+
+    private fun initWaterDialog()
+    {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_give_water, null)
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        val alertDialog = builder.create()
+        binding.giveWaterBtn.setOnClickListener {
+            showGiveWaterDialog(dialogView, alertDialog)
+        }
+
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        alertDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alertDialog.setContentView(R.layout.dialog_give_water)
+    }
+
+    private fun showGiveWaterDialog(view : View, dialog: AlertDialog)
+    {
+        dialog.show()
+        view.findViewById<AppCompatButton>(R.id.cancel_btn).setOnClickListener {
+            dialog.dismiss()
+        }
+        view.findViewById<AppCompatButton>(R.id.apply_btn).setOnClickListener {
+            val prefs = MyApplication.pref
+            val waterValue = view.findViewById<EditText>(R.id.give_et).text.toString()
+            if(waterValue != null && waterValue.toInt() > 0){
+                prefs.setNow(prefs.getNow() + waterValue.toInt())
+                viewModel.valueChange()
+            }
+            dialog.dismiss()
+        }
+    }
     private fun initSlider()
     {
         val cancelBtn = settingContentView.findViewById<AppCompatButton>(R.id.cancel_button)
@@ -75,12 +150,12 @@ class MainActivity : AppCompatActivity() {
 
             val nowDrink = settingContentView.findViewById<EditText>(R.id.now_et).text.toString()
             val goalDrink = settingContentView.findViewById<EditText>(R.id.goal_et).text.toString()
+
             if(goalDrink == "0") Toast.makeText(this,"목표는 0이 될 수 없습니다",Toast.LENGTH_SHORT).show()
             else
             {
                 binding.apply {
-                    nowTv.text = nowDrink
-                    goalTv.text = goalDrink
+
                     var progress : Int = if(nowDrink == "0") 0 else (nowDrink.toInt() * 100) / goalDrink.toInt()
                     if(progress > 100) progress = 100
                     waveView.setProgress(progress)
@@ -89,6 +164,7 @@ class MainActivity : AppCompatActivity() {
                 val prefs = MyApplication.pref
                 prefs.setNow(nowDrink.toInt())
                 prefs.setGoal(goalDrink.toInt())
+                viewModel.valueChange()
                 settingSlideUpPopup.dismissAnim()
             }
         }
--- app/src/main/java/com/shino72/waterplant/main/MainViewModel.kt
+++ app/src/main/java/com/shino72/waterplant/main/MainViewModel.kt
@@ -12,16 +12,21 @@ class MainViewModel : ViewModel() {
     private val _name = MutableLiveData<String>()
     private val _wave = MutableLiveData<Int>()
 
-    val now : LiveData<Int> get() = _now
-    val goal : LiveData<Int> get() = _goal
-    val name : LiveData<String> get() = _name
-    val wave : LiveData<Int> get() = _wave
-    init {
+    val now: LiveData<Int> get() = _now
+    val goal: LiveData<Int> get() = _goal
+    val name: LiveData<String> get() = _name
+    val wave: LiveData<Int> get() = _wave
+
+    fun valueChange()
+    {
         _now.value = MyApplication.pref.getNow()
         _goal.value = MyApplication.pref.getGoal()
         _name.value = MyApplication.pref.getName()
-
         val progress = if(_now.value == 0) 0 else (_now.value!! * 100) / _goal.value!!
         _wave.value = if(progress > 100) 100 else progress
+    }
+
+    init {
+        valueChange()
     }
 }
